97e00724abb8f0a0da3ce562b063fbb1ba4e423a
DEVGIT-farhan <farhanfarhan.in@gmail.com>
2026-08-24 15:52:40 +0530

    Deploy with GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: isGitHubPages ? "/grootz-financial" : "",
+};
 
 export default nextConfig;
